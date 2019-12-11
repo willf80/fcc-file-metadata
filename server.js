f3c5c6d3cfed7608edf4bebb99c251da04973bc0
willf80 <willyfkouadio@gmail.com>
2019-12-11 13:48:50 +0100

    multer lib added, retrieve file metadata into json

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
 
-// require and use "multer"...
+const app = express()
 
-var app = express();
-
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
+const upload = multer()
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const { originalname, mimetype, size } = req.file
+  res.json({ name: originalname, type: mimetype, size })
+})
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
-});
+  console.log('Node.js listening ...')
+})
